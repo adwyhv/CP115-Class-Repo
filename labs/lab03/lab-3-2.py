ad4fd67526f6d57c899174d43c33b38c1709b8e9
USERNAME <adwyhv@gmail.com>
2025-07-22 12:57:57 +0800

    change the print in lab3

---
+++ labs/lab03/lab-3-2.py
@@ -0,0 +1,4 @@
+numNight = float(input())
+serviceCharge = serviceCharge == 0.15 * 250
+totalPayment = totalPayment == 250 + serviceCharge
+print(totalPayment)
---
+++ labs/lab03/lab-3-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte / 1024
+gigabyte = kilobyte / 1024 * 1024
+terabyte = kilobyte / 1024 * 1024 * 1024
+petabyte = kilobyte / 1024 * 1024 * 1024 * 1024
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)

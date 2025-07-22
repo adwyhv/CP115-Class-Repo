f35327e64fb00a7af7cc07ea9b8af74365936438
USERNAME <adwyhv@gmail.com>
2025-07-22 23:53:32 +0800

    change the print in lab03

---
+++ labs/lab03/lab-3-6.py
@@ -0,0 +1,8 @@
+builtInHouseLength = input()
+builtInHouseWidth = input()
+houseLength = input()
+housewidth = input()
+builtInHouseArea = builtInHouseLength * builtInHouseWidth
+houseArea = houseLength * housewidth
+mowYard = houseArea - builtInHouseArea
+wage = mowYard * 2

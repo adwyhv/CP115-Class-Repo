508b57e7e4e767f8605cb2fefda1386d4f1e151f
USERNAME <adwyhv@gmail.com>
2025-08-05 12:40:28 +0800

    change print in lab05

--- labs/lab05/exercise.py
+++ labs/lab05/exercise.py
@@ -1,2 +1,14 @@
-# Lab 05 Exercise File
-# This file serves as a placeholder for lab05 exercises
+# Basic input and output
+print("Welcome!")
+print("This program will show your data.")
+
+# Getting user input (always returns a string)
+name = input("Enter your name: ")
+course_code = input("Enter the course code: ")
+# Getting user input(always returnd a numeric)
+age = input("enter your age: ")
+# Formatted output using string concatenation
+print()
+print("Student: " + name)
+print("age: " + age)
+print("Course: " + course_code)

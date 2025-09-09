92382da5f73fa9eeed7d81029094093f786a83c5
USERNAME <adwyhv@gmail.com>
2025-09-09 12:10:58 +0800

    change the print in lab09

--- labs/lab09/exercise3/exercise3.py
+++ labs/lab09/exercise3/exercise3.py
@@ -2,33 +2,34 @@ day_type = input()
 show_time = int(input())
 customer_type = input()
 is_student = input()
+evening_show= 3
 
 if day_type == "Weekday" and show_time > 18 and is_student == "Yes" and customer_type == "Adult":
     base_price = 15 + 3
     final_price = base_price * 0.9
 elif day_type == "Weekday" and show_time >18 and is_student == "Yes" and customer_type == "Child":
     base_price = 10 + 3
-    final_price = base_price *0.9
+    final_price =base_price *0.9
 elif day_type == "Weekday" and show_time >18 and is_student == "Yes" and customer_type =="Senior":
     base_price = 12 + 3
     final_price = base_price*0.9
 elif day_type == "Weekday" and show_time < 18 and is_student == "Yes" and customer_type =="Adult":
-    base_price=15
+    base_price = 15
     final_price = base_price * 0.9
 elif day_type == "Weekday" and show_time < 18 and is_student == "Yes" and customer_type =="Child":
     base_price =10
     final_price = base_price*0.9
 elif day_type == "Weekday" and show_time < 18 and is_student =="Yes" and customer_type == "Customer":
-    base_price = 12
-    final_price = base_price * 0.9
+   base_price = 12
+   final_price = base_price * 0.9
 
 
 if day_type == "Weekday" and show_time >18 and is_student == "No" and customer_type == "Adult":
     base_price = 15 + 3
     final_price = base_price
 elif day_type =="Weekday" and show_time > 18 and is_student =="No" and customer_type == "Child":
-    base_price = 10 + 3
-    final_price = base_price
+   base_price = 10 + 3
+   final_price = base_price
 elif day_type == "Weekday" and show_time > 18 and is_student =="No" and customer_type == "Senior":
     base_price = 12 + 3
     final_price = base_price
@@ -37,32 +38,32 @@ if day_type == "Weekday" and show_time <18 and is_student == "No" and customer_t
     base_price = 15
     final_price = base_price
 elif day_type =="Weekday" and show_time < 18 and is_student =="No" and customer_type == "Child":
-    base_price = 10 
-    final_price = base_price
+   base_price = 10 
+   final_price = base_price
 elif day_type == "Weekday" and show_time < 18 and is_student =="No" and customer_type == "Senior":
-    base_price = 12
-    final_price = base_price
+   base_price = 12
+   final_price = base_price
 
 
 if day_type == "Weekend" and show_time >18 and is_student=="No" and customer_type =="Adult":
-    base_price = 18 + 3
-    final_price = base_price
+   base_price = 18 + 3
+   final_price = base_price
 elif day_type == "Weekend" and show_time >18 and is_student=="No" and customer_type =="Child":
-    base_price = 12 + 3
-    final_price = base_price
+   base_price = 12 + 3
+   final_price = base_price
 elif day_type == "Weekend" and show_time >18 and is_student=="No" and customer_type=="Senior":
-    base_price = 15 + 3
-    final_price = base_price
+   base_price = 15 + 3
+   final_price = base_price
 
 if day_type == "Weekend" and show_time <18 and is_student=="No" and customer_type =="Adult":
     base_price = 18 
     final_price = base_price
 elif day_type == "Weekend" and show_time <18 and is_student=="No" and customer_type =="Child":
-    base_price = 12 
-    final_price = base_price
+   base_price = 12 
+   final_price = base_price
 elif day_type == "Weekend" and show_time <18 and is_student=="No" and customer_type=="Senior":
-    base_price = 15 
-    final_price = base_price
+   base_price = 15 
+   final_price = base_price
 
 
 print(base_price)

838701b64383e3a0089fa4ed0b76d980a1e172fe
USERNAME <adwyhv@gmail.com>
2025-09-23 12:22:34 +0800

    change the print

--- labs/lab10/exercise3/exercise3.py
+++ labs/lab10/exercise3/exercise3.py
@@ -2,8 +2,8 @@ monthly_income = int(input())
 credit_score = int(input())
 loan_amount = int(input())
 
-# Your code here
-
+if income_requirement >= 4000 :
+    
 print(interest_rate)
 print(max_loan_amount)
 print(approval_status)
